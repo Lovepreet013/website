fef3ee88f4448780837bbb8a7ee93b22100c97b4
Lovepreet013 <singhlovepreet1209@gmail.com>
2024-07-03 19:11:51 +0530

    make responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Image from "next/image";
 import Link from "next/link";
 import { RoughNotation } from "react-rough-notation";
@@ -10,9 +11,18 @@ const RoughComp = (text : String, color : any, animationDelay : number, type = '
     )
 }
 
+const getWidth = () =>{
+    const w = window.innerWidth
+    if(w == 360) return w
+    if(w == 540) return w
+    if(w == 768) return w
+    console.log(w);
+}
+getWidth()
+
 const Hero = () => {
     return (
-        <div className="flex flex-col md:flex-row lg:flex-row pt-10 pb-20 mx-auto max-w-5xl justify-between px-8 tracking-tight min-[784px]:flex-col items-center" id="about">
+        <div className="flex flex-col md:flex-row lg:flex-row pt-10 pb-20 mx-auto max-w-5xl justify-between px-8 tracking-tight items-center" id="about">
             <div className="space-y-4 mr-6">
                 <h1 className="text-4xl font-bold">
                     Hello! I'm Lovepreet, a
@@ -21,28 +31,30 @@ const Hero = () => {
                 </h1>
                 <p>
                     I love building tools that are
-                    {RoughComp('user friendly, simple', '#e3ae83', 500)}
+                    <span className={`min-[375px]:inline-block min-[500px]:inline ${getWidth()? `inline-block` : ''}`}>
+                        {RoughComp('user friendly, simple', '#e3ae83', 500)}
+                    </span>
                     and delightful.
                 </p>
                 <p>
                     I am a Mechanical Eng. student fall in love with
-                    <span className="block min-[780px]:inline lg:inline">
+                    <span className={`${getWidth() ? 'inline-block' : 'inline'}`}>
                         {RoughComp('Frontend-Development.', '#d095e8', 1000)}
                         I have experience in developing innovative web applications using technologies such as Javascript, React.js and Next.js.
                     </span>
                 </p>
                 <p>
                     I'm currently looking for a new role as developer.
-                    <span className="block md:inline lg:inline">
+                    <span className={`${getWidth() ? 'inline' : ''}`}>
                         {RoughComp('Hire me?', '#b61af3', 1500, 'circle')}
                     </span>
                 </p>
-                <div className=" space-x-3">
+                <div className="flex-col sm:flex-row ">
                     <Link href='https://www.linkedin.com/in/lovepreetsingh013/' target="_blank">
-                        <button className="border rounded-md px-6 py-2 bg-black text-white hover:bg-gray-800">View Linkedin</button>
+                        <button className="border rounded-md px-6 py-2 mr-2 mt-4 sm:mr-4 bg-black text-white hover:bg-gray-800">View Linkedin</button>
                     </Link>
                     <Link href='https://github.com/Lovepreet013' target="_blank">
-                        <button className="border px-6 py-2 border-black/20 rounded-md">View Github</button>
+                        <button className="border px-7 py-2 border-black/20 rounded-md mt-3">View Github</button>
                     </Link>
                 </div>
             </div>
